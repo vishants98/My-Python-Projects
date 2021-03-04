bd9816ce487865cfc0b10797f58a79a25ee25054
Vishant Sharma <vishants98@gmail.com>
2021-03-04 10:10:50 +0530

    Update scraper.py

--- Google Image Scrapper/scraper.py
+++ Google Image Scrapper/scraper.py
@@ -92,13 +92,6 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
         counter += 1
 
 
-# How to execute this code
-# Step 1 : pip install selenium. pillow, requests
-# Step 2 : make sure you have chrome installed on your machine
-# Step 3 : Check your chrome version ( go to three dot then help then about google chrome )
-# Step 4 : Download the same chrome driver from here  " https://chromedriver.storage.googleapis.com/index.html "
-# Step 5 : put it inside the same folder of this code
-
 
 DRIVER_PATH = './chromedriver.exe'
 search_term = 'iphone'
