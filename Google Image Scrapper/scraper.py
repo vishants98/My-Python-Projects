d60788faeaaeace1eb410b82720362c74510da2f
Vishant Sharma <vishants98@gmail.com>
2021-03-04 10:11:30 +0530

    Update scraper.py

--- Google Image Scrapper/scraper.py
+++ Google Image Scrapper/scraper.py
@@ -98,3 +98,18 @@ search_term = 'iphone'
 # num of images you can pass it from here  by default it's 10 if you are not passing
 # number_images = 10
 search_and_download(search_term=search_term, driver_path=DRIVER_PATH)
+
+
+
+
+
+
+
+
+# How to execute this code
+# Step 1 : pip install selenium. pillow, requests
+# Step 2 : make sure you have chrome installed on your machine
+# Step 3 : Check your chrome version ( go to three dot then help then about google chrome )
+# Step 4 : Download the same chrome driver from here  " https://chromedriver.storage.googleapis.com/index.html "
+# Step 5 : put it inside the same folder of this code
+
